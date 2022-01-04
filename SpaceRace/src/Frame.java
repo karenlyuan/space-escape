a2d54f9d4ecc2994d14b8b2be430609baa3a471e
1860622 <1860622@01-19962.pusd.dom>
2022-01-04 09:15:07 -0800

    setup for space race

---
+++ SpaceRace/src/Frame.java
@@ -0,0 +1,112 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		
+	}
+	
+	public static void main(String[] arg) {
+		Frame f = new Frame();
+	}
+	
+	public Frame() {
+		JFrame f = new JFrame("Space Race");
+		f.setSize(new Dimension(900, 600));
+		f.setBackground(Color.BLACK);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		
+
+	}
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- SpaceRace/src/Rocket.java
+++ SpaceRace/src/Rocket.java
@@ -1,4 +1,98 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
 
 public class Rocket {
+	
+	//location attributes for left rocket
+	public static int x1 = 220;
+	public static int y1 = 145;
+	
+	//location attributes for right rocket
+	public static int x2 = 680;
+	public static int y2 = 145;
+	
+	private int length, width; //size
+	public static double speedX = 5;
+	public static double speedY = 5;
+	
+	public static Image img;
+	private Image background;
+	private static AffineTransform tx;
+	public boolean draw;
+
+	public Rocket() {
+		
+		img = getImage("rocket-transparent");
+		
+		tx = AffineTransform.getTranslateInstance(x1, y1);
+		
+		init(x1, y1); 				//initialize the location of the image
+										//use your variables
+		init(x2, y2);
+		
+	}
+	
+	public void changePicture(String newFileName) {
+		img = getImage(newFileName);
+		init(x1, y1);
+		init(x2, y2);
+		
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(img, tx, null);
+		
+		x1 += speedX;
+		y1 += speedY;
+		
+		update();
+				
+		if (x1 <= -75 || x1 >= 780) {
+			speedX *= -1;
+		}
+		
+		if (y1 <= -30 || y1 >= 375) {
+			speedY *= -1;
+		}
+
+	}
+	
+	public void update() {
+		tx.setToTranslation(x1, y1);
+		tx.setToTranslation(x2, y2);
+		tx.scale(0.1, 0.1);
+	}
+	
+	private static void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(1, 1);
+	}
+
+	public static Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Rocket.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+
+	public static void reset() {
+		img = getImage("rocket-transparent");
+		
+		tx = AffineTransform.getTranslateInstance(x1, y1);
+		init(x1, y1);
+		init(x2, y2);
+	}
 
 }
