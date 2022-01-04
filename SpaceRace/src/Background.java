8626db8c9be981b3b8e54a3bccb509e049a4dc9a
1860622 <1860622@01-19962.pusd.dom>
2022-01-04 08:50:39 -0800

    space race game setup

---
+++ SpaceRace/src/Background.java
@@ -0,0 +1,15 @@
+
+public class Background {
+
+}
+
+
+/*
+Notes for project:
+- connect with github
+- use previous projects as templates for classes!
+- use own assets for game (pictures and sounds)
+- find graphics or create them
+- set up classes and code game!
+
+*/
