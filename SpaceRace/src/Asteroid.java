d16b73b7a6a9a1f1f82b74e43fa03fa9b165bc54
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-01-18 16:02:39 -0800

    Update Asteroid.java

--- SpaceRace/src/Asteroid.java
+++ SpaceRace/src/Asteroid.java
@@ -5,90 +5,50 @@ public class Asteroid {
 	
 	//instance variables should be private
 	private int x, y; //position
-	private int size; //size of asteroids
-	private int vx, vy;
+	private int width; //size of asteroids
+	private int vx;
 	private Color color;
 	private double area; //helps with width calculation
 							//as we consume other cells
 	
 	
 	public Asteroid(int x, int y) {
-		
-	}
-}
-
-/*
-public class Cell {
-
-	
-	//non default constructor - with parameters
-	public Cell(int newX, int newY) {
-		this();
-		x = newX;
-		y = newY;
+		this.x = x;
+		this.y = y;
 	}
 	
-	//Cell myCell = new Cell(50,50);
+	Asteroid myAsteroid = new Asteroid(100, 100);
 	
-	//default constructor - no parameters
-	public Cell() {
+	public Asteroid() {
+		width = 10;
+		x = 900;
+		y = (int)(Math.random()*(450+1));
 		
-		// (int)(Math.random()*(range+1))+min;
-		width = (int)(Math.random()*(30-10+1))+10;
-		x = 800/2-width/2;
-		y = 600/2-width/2;
+		color = new Color(255,255,255);
 		
-		//randomize color
-		int red = (int)(Math.random()*256);
-		int green = (int)(Math.random()*256);
-		int blue = (int)(Math.random()*256);
-		
-		color = new Color(red, green, blue);
-		
-		vx = (int)(Math.random()*7)-3;
-		vy = (int)(Math.random()*7)-3;
-		
-		//scale the velocity such that the bigger the object is
-		//the smaller vx and vy are initially
-		
-		//assign area
-		area = Math.PI*Math.pow(width/2, 2);
-		
-		if (vx == 0) {vx = 1;}
-		
+		vx = (int)(Math.random()*(11))+10;
 	}
 	
 	public void Paint(Graphics g) {
-		y+=vy;
-		x+=vx;
+		x-=vx;
 		g.setColor(color);
 		g.fillOval(x,y,width,width);
 	}
 	
-	public boolean Collide(Cell other) {
+	public boolean Collide(Asteroid a) {
 		
-		int x1 = this.x+width/2;
-		int y1 = this.y+width/2;
+		//if robot collides with an Asteroid
 		
-		int x2 = other.x+other.width/2;
-		int y2 = other.y+other.width/2;
-		
-		int d = (int)(Math.sqrt(Math.pow(x1-x2, 2)+Math.pow(y1-y2,2)));
-		int r1 = this.width/2;
-		int r2 = other.width/2;
-		
-		//figure out which cell is bigger
-		//increment its size somehow
-		if(this.area > other.area) {
-			//add other area to this area
-			this.area += other.area; //eat other cell
-			
-			//calculate new width based on new area
-			this.width = (int) Math.sqrt(this.area/Math.PI)*2;
-			
+		if(a.getX() + width >= Rocket.getX1() && a.getX() - width <= Rocket.getX1()
+				&& a.getY() + width >= Rocket.getY1() && a.getY() - width <= Rocket.getY1()) {
+			return true;
+		} else if(a.getX() + width >= Rocket.getX2() && a.getY() - width <= Rocket.getX2()
+				&& a.getY() + width >= Rocket.getY1() && a.getY() - width <= Rocket.getY1()) {
+			return true;
+		} else {
+			return false;
 		}
 		
-		return false&&(r1+r2)>d;
 	}
 	
 	//getter
@@ -124,14 +84,6 @@ public class Cell {
 	public void setVx(int vx) {
 		this.vx = vx;
 	}
-
-	public int getVy() {
-		return vy;
-	}
-
-	public void setVy(int vy) {
-		this.vy = vy;
-	}
 	
 	public Color getColor() {
 		return color;
@@ -142,4 +94,3 @@ public class Cell {
 	}
 
 }
-*/
