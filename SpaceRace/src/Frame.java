2b5bd8aabcf085e8ad8726a669bd510beb2ed06e
karenlyuan <65468255+karenlyuan@users.noreply.github.com>
2022-02-05 23:30:20 -0800

    Update Frame.java

--- SpaceRace/src/Frame.java
+++ SpaceRace/src/Frame.java
@@ -4,10 +4,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.MouseInfo;
 import java.awt.Point;
-import java.awt.PointerInfo;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,11 +12,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
@@ -27,59 +22,91 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
-//game reference: https://dev.to/lukegarrigan/top-5-best-games-to-code-as-a-beginner-9n
-
+/*add more levels and hit detection
+ * clean graphics but cannot progress past level two, no hit detection +1
+ * yeah cannot go past level 2
+ * nice graphics
+ * cool game
+ * hit detection is a little weird
+ * Apparently I beat the game at level 2? Also I might be invincible ? pretty cool otherwise ?
+ * am i... better than everyone else?
+ */
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+	}
 	
-	int score1 = 0;
-	int score2 = 0;
+	Background background = new Background(0,0);
 	
-	private ArrayList<Asteroid> asteroids = new ArrayList<Asteroid>();
+	Ship ship1 = new Ship();
 	
-	Background bkgrd = new Background(0,0);
-	Asteroid myAsteroid = new Asteroid(100, 100);
+	Start start = new Start(0, 0);
+	
+	int aX = 800;
+	int aY = 0;
+	int speedY = 5;
+	int level = 1;
+	int s = 0;
+	int variable1 = 10;
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.black);
-		g.fillRect(0,0,1000,1000);
-		bkgrd.paint(g);
-		myAsteroid.Paint(g);
 		
+		start.paint(g);
+		if(s == 10) {
+		background.paint(g);
 		
-		Font c = new Font("Helvetica", Font.BOLD, 70);
-		g.setFont(c);
+		ship1.paint(g);
+		ship1.sX1 += ship1.sX1Code*3;
+		ship1.sY1 += ship1.sY1Code*3;
+		if(ship1.sX1 <= 0) {
+			ship1.sX1 += 3;
+		}
+		if(ship1.sY1 <= 0) {
+			ship1.sY1 += 3;
+		}
+		if(ship1.sY1 >= 525) {
+			ship1.sY1 -= 3;
+		}
+		
+		aY += speedY;
+		
+		if(aY == 600) {
+			aY=0;
+		}
 		g.setColor(new Color(255,255,255));
-		g.drawString(score1+"", 220, 500);
+		Font c = new Font("Courier New", Font.BOLD, 30);
+		g.setFont(c);
+		g.drawString("Level "+level, 20, 40);
 		
-		if(Rocket.y1 == 0) {
-			score1++;
+		Asteroid asteroid = new Asteroid(800, aY);
+		Asteroid asteroid2 = new Asteroid(700, aY);
+		Asteroid asteroid3 = new Asteroid(600, aY);
+		Asteroid asteroid4 = new Asteroid(500, aY);
+		Asteroid asteroid5 = new Asteroid(400, aY);
+		Asteroid asteroid6 = new Asteroid(300, aY);
+		Asteroid asteroid7 = new Asteroid(200, aY);
+		asteroid.Paint(g);
+		asteroid2.Paint(g);
+		asteroid3.Paint(g);
+		asteroid4.Paint(g);
+		asteroid5.Paint(g);
+		asteroid6.Paint(g);
+		asteroid7.Paint(g);
+		g.setColor(new Color(255,255,255));
+		
+		for(int s = 0; s < 15; s++) {
+		if(ship1.sX1 == 800) {
+			ship1.sX1 = 0;
+			level++;
 		}
 		
-		if(score1 == 5) {
-			//set screen to the game over screen
-			//over.paint(g);
-			/*if(score1 > score2) {
-				g.drawString("Player 1 Wins!", 420, 270);
-			}
-			if(score2 > score1) {
-				g.drawString("Player 2 Wins!", 420, 270);
-			} */
 		}
+			
 		
-		g.setColor(Color.white);
-		for(Asteroid a: asteroids) {
-			a.Paint(g);
 		}
-		
-		
 	}
-	
-	public static void main(String[] arg) {
-		Frame f = new Frame();
-	}
-	
+	//change
 	public Frame() {
 		JFrame f = new JFrame("Space Race");
 		f.setSize(new Dimension(900, 600));
@@ -89,24 +116,11 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 		f.setLayout(new GridLayout(1,2));
 		f.addMouseListener(this);
 		f.addKeyListener(this);
-		Timer t = new Timer(16, this);
+		Timer t = new Timer(10, this);
 		t.start();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
-		
-		BufferedImage cursorImg;
-		try {
-			cursorImg =  ImageIO.read(new File("aiming-cursor.png"));
-			Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(
-				    cursorImg, new Point(0, 0), "blank cursor");
-			f.getContentPane().setCursor(blankCursor);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
 	}
-	
 	
 	
 	@Override
@@ -145,36 +159,46 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	}
 
 	@Override
-	public void keyPressed(KeyEvent m) {
-		System.out.println(m.getKeyCode());
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		//System.out.println(arg0.getKeyCode());
+		if (arg0.getKeyCode() == 83) {
+			s = 10;
+		} 
 		
-		//w = 87
-		//s = 83
-		//up arrow = 38
-		//down arrow = 40
+		if (arg0.getKeyCode() == 87) {
+			ship1.sY1Code = -1;
+		} 
+		if (arg0.getKeyCode() == 83) {
+			ship1.sY1Code = 1;
+		} 
 		
-		if(m.getKeyCode() == 38) {
-			Rocket.y1 -= 5;
-		}
-		
-		if(m.getKeyCode() == 40) {
-			Rocket.y1 += 5;
-		}
-		
-		if(m.getKeyCode() == 39) {
-			Rocket.x1 += 5;
-		}
-		
-		if(m.getKeyCode() == 37) {
-			Rocket.x1 -= 5;
-		}
+		if (arg0.getKeyCode() == 65) {
+			ship1.sX1Code = -1;
+		} 
+		if (arg0.getKeyCode() == 68) {
+			ship1.sX1Code = 1;
+		} 
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
+		if (arg0.getKeyCode() == 87) {
+			ship1.sY1Code = 0;
+		} 
+		if (arg0.getKeyCode() == 83) {
+			ship1.sY1Code = 0;
+		} 
 		
+		if (arg0.getKeyCode() == 65) {
+			ship1.sX1Code = 0;
+		} 
+		if (arg0.getKeyCode() == 68) {
+			ship1.sX1Code = 0;
+			
+		} 
 	}
 
 	@Override
